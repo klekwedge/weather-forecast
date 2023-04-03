8dc970a4281bb5dde9a70a0da1024c43a77e7cab
Andrey <antonov.andrey.10101@gmail.com>
2023-04-03 18:13:14 +0300

    feat: change index.html

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -1,18 +1,22 @@
-import { lazy, Suspense } from "react";
-import { Box } from "@chakra-ui/react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Spinner from "../Spinner/Spinner";
+import { lazy, Suspense } from 'react';
+import { Box } from '@chakra-ui/react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Spinner from '../Spinner/Spinner';
 
-const CityListPage = lazy(() => import("../../pages/CityListPage"));
-const CityPage = lazy(() => import("../../pages/CityPage"));
-const MyLocationPage = lazy(() => import("../../pages/MyLocationPage"));
-const Page404 = lazy(() => import("../../pages/Page404/Page404"));
+const CityListPage = lazy(() => import('../../pages/CityListPage'));
+const CityPage = lazy(() => import('../../pages/CityPage'));
+const MyLocationPage = lazy(() => import('../../pages/MyLocationPage'));
+const Page404 = lazy(() => import('../../pages/Page404/Page404'));
 
 function App() {
-  return ( 
+  return (
     <Router>
-      <Box maxW='1200px' margin='0 auto' className="flex flex-col items-center">
-        <Suspense fallback={<Spinner/>}>
+      <Box
+        maxW="1200px"
+        margin="0 auto"
+        className="flex flex-col items-center"
+      >
+        <Suspense fallback={<Spinner />}>
           <Routes>
             <Route path="/" element={<CityListPage />} />
             <Route path="/:cityId" element={<CityPage />} />
--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,14 @@
-import { ChakraProvider } from "@chakra-ui/react";
-import ReactDOM from "react-dom/client";
-import { Provider } from "react-redux";
+import { ChakraProvider } from '@chakra-ui/react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import store from './store/store';
-import "./index.scss";
-import App from "./components/App/App";
+import './index.scss';
+import App from './components/App/App';
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
-
-root.render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <Provider store={store}>
     <ChakraProvider>
       <App />
     </ChakraProvider>
-  </Provider>
+  </Provider>,
 );

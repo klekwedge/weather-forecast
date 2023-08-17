8aac225402ab3b64df9fbd4b544ed0be73154961
Andrey <antonov.andrey.10101@gmail.com>
2023-08-17 08:54:40 +0400

    refactor: move api key to env file

--- src/services/OpenWeatherApi.ts
+++ src/services/OpenWeatherApi.ts
@@ -1,5 +1,6 @@
+const apiKey = import.meta.env.VITE_API_KEY
+
 function OpenWeather() {
-  const apiKey = 'be747318592dd14aa7030afcf31e3799';
 
   function getCity(cityName: string) {
     return `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`;
